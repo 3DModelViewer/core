eb9a3244cd0aec85e1a818d6a8b45eb92e16d50b
Daniel Robinson <dan.rob.xor@gmail.com>
2016-02-29 16:57:33 +0000

    pass in connection string

--- sql_impl.go
+++ sql_impl.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/modelhub/core/documentversion"
 	"github.com/modelhub/core/project"
 	"github.com/modelhub/core/sheet"
@@ -12,11 +13,15 @@ import (
 	"time"
 )
 
-func NewSqlCoreApi(db *sql.DB, vada vada.VadaClient, statusCheckTimeout time.Duration, ossBucketPrefix string, ossBucketPolicy vada.BucketPolicy, log golog.Log) (CoreApi, error) {
-	us := user.NewSqlUserStore(db, log)
-	ps := project.NewSqlProjectStore(db, vada, ossBucketPrefix, ossBucketPolicy, log)
-	tns := treenode.NewSqlTreeNodeStore(db, vada, ossBucketPrefix, log)
-	dvs := documentversion.NewSqlDocumentVersionStore(db, statusCheckTimeout, vada, ossBucketPrefix, log)
-	ss := sheet.NewSqlSheetStore(db, vada, log)
-	return newCoreApi(us, ps, tns, dvs, ss)
+func NewSqlCoreApi(mySqlConnection string, vada vada.VadaClient, statusCheckTimeout time.Duration, ossBucketPrefix string, ossBucketPolicy vada.BucketPolicy, log golog.Log) (CoreApi, error) {
+	if db, err := sql.Open("mysql", mySqlConnection); err != nil {
+		return nil, err
+	} else {
+		us := user.NewSqlUserStore(db, log)
+		ps := project.NewSqlProjectStore(db, vada, ossBucketPrefix, ossBucketPolicy, log)
+		tns := treenode.NewSqlTreeNodeStore(db, vada, ossBucketPrefix, log)
+		dvs := documentversion.NewSqlDocumentVersionStore(db, statusCheckTimeout, vada, ossBucketPrefix, log)
+		ss := sheet.NewSqlSheetStore(db, vada, log)
+		return newCoreApi(us, ps, tns, dvs, ss)
+	}
 }
